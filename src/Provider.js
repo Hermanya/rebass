96619c38cbec3b99cf3595b996cea68f6fe3404a
Herman Starikov <hermanstarikov@gmail.com>
2018-08-26 21:19:02 -0400

    remove root

--- src/Provider.js
+++ src/Provider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import Root from './Root'
+import { View } from 'react-primitives'
 import defaultTheme from './theme'
 
 export class Provider extends React.Component {
@@ -12,7 +12,7 @@ export class Provider extends React.Component {
 
     return (
       <ThemeProvider theme={{ ...defaultTheme, ...theme }}>
-        <Root {...props} />
+        <View {...props} />
       </ThemeProvider>
     )
   }
